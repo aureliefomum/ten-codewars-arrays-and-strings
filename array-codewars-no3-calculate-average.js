3875b9aaa200543260263538d33ea19ba7a920cc
aureliefomum <azam_aurelie@yahoo.com>
2021-05-11 20:32:54 -0300

    function calculates average of list of numbers

---
+++ array-codewars-no3-calculate-average.js
@@ -0,0 +1,10 @@
+// CODEWARS 8KYU - Calculate average
+// Write a function which calculates the average of the numbers in a given list.
+
+//     Note: Empty arrays should return 0
+
+// Solution 1:
+function find_average(array) {
+    return array.length === 0 ? 0 : array.reduce((acc, c) => acc + c, 0) / array.length
+
+}
