f840e1cdcc5a11588586236e1065d924eb880cef
aureliefomum <azam_aurelie@yahoo.com>
2021-05-11 20:38:32 -0300

    checks for item in list

---
+++ array-codewars-no4-enumerable-magic.js
@@ -0,0 +1,8 @@
+// CODEWARS 8Kyu Enumerable Magic #3 - Does My List Include This?
+// INSTRUCTIONS
+// Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+// SOLUTION:
+function include(arr, item){
+    return arr.includes(item)
+  }
